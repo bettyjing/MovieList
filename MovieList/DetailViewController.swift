cc4ff0c145a9fa543db37a4086b2833d0d41121d
Betty Jing <bettyjing@Bettys-MacBook-Air.local>
2016-02-06 21:51:22 -0700

    detailZoom

--- MovieList/DetailViewController.swift
+++ MovieList/DetailViewController.swift
@@ -13,15 +13,21 @@ class DetailViewController: UIViewController {
     @IBOutlet var overviewLabel: UILabel!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var posterImageView: UIImageView!
-  
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var infoView: UIView!
+    
     var movie: NSDictionary!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
         let title = movie["title"] as? String
         titleLabel.text = title
         let overview = movie["overview"]
         overviewLabel.text = overview as?String
+        overviewLabel.sizeToFit()
         
         
         let baseUrl = "http://image.tmdb.org/t/p/w500"
